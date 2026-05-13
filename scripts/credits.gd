2c9ef51adf902a0271ff7d99b5ff0613b4a13ec4
jack-frings <jackdfrings@protonmail.com>
2026-05-13 01:07:30 -0700

    refined credits

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var title = $Title
+@onready var tile_bg = $TileBackground
 @onready var credits = $Credits
 @onready var positive = $Positive
 @onready var negative = $Negative
@@ -12,6 +13,7 @@ var credits_y = 192
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	title.visible = false
+	tile_bg.visible = false
 	positive.visible = false
 	negative.visible = false
 	final_message.visible = false
@@ -24,10 +26,11 @@ func _process(delta: float) -> void:
 	time += delta 
 	if time > 1.0:
 		title.visible = true
+		tile_bg.visible = true
 	if time > 1.5:
 		credits_y = 192 - (time-1.5)*80
-		if credits_y < 50:
-			credits_y = 50
+		if credits_y < 65:
+			credits_y = 65
 		credits.position = Vector2(90, credits_y)
 	if time > 3.5:
 		positive.visible = true
